dabec1c4d9fff6cb219cb8a380a150609b0905d5
Zildanmajidaminuddin <zildanmajid16@gmail.com>
2025-02-14 00:37:36 +0700

    mengerjakan tugas 1

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,9 @@
+<?php
+  $nama ="zildan majid";
+  $email="zildanmajid16@gmail.com";
+  $pekerjaan="mahasiswa";
+  $jurusan="teknik informatika ";
+  $universitas="unpas";
+  echo "<H1>$nama</H1>";
+  echo "<H4>$email</H4>";
+  echo "<P>haloo saya $nama saya $pekerjaan di $universitas $jurusan saya semester 1</p";
