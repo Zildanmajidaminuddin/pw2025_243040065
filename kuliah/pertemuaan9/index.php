eb2eb997c129527cd78763596faa844994285826
Zildanmajidaminuddin <zildanmajid16@gmail.com>
2025-05-15 20:59:17 +0700

    selesai mengerjakan latihan

--- kuliah/pertemuaan9/index.php
+++ kuliah/pertemuaan9/index.php
@@ -21,7 +21,7 @@ $result = mysqli_query($conn, "SELECT * FROM mahasiswa");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Admin</title>
+    <title>Halaman Adminn</title>
 </head>
 <body>
     
